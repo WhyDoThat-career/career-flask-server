97bfa2108c4e1001b62f748b069a40c1ac4d6e31
Keunyoung-Jung <jgy206@gmail.com>
2021-03-10 16:02:57 +0900

    jenkins test

--- run_server.py
+++ run_server.py
@@ -3,7 +3,7 @@ from admin.control.user_mgmt import registerAdmin
 
 db.create_all()
 registerAdmin()
-print('jenkins test2')
+print('jenkins test3')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888',debug=True)
