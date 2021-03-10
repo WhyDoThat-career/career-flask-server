50b80652fb99099f066ecbb1c1c600254e8589cc
Ubuntu <ubuntu@ip-172-31-43-1.ap-northeast-2.compute.internal>
2021-03-10 06:54:47 +0000

    jenkins test

--- run_server.py
+++ run_server.py
@@ -3,6 +3,7 @@ from admin.control.user_mgmt import registerAdmin
 
 db.create_all()
 registerAdmin()
+print('jenkins test')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888',debug=True)
