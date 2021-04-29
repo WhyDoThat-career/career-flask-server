c87e2600330739351cb3ff51d033f8994a3242e6
Keunyoung-Jung <jgy206@gmail.com>
2021-04-29 14:25:15 +0900

    s3 동기화 작업 manage로 이동

--- manage.py
+++ manage.py
@@ -3,6 +3,17 @@ from flask_migrate import Migrate,MigrateCommand
 from flask_script import Manager
 import sqlalchemy_utils
 
+def downloadDirectoryFroms3(bucketName,remoteDirectoryName):
+    s3_resource = boto3.resource('s3')
+    bucket = s3_resource.Bucket(bucketName) 
+    for object in bucket.objects.filter(Prefix = remoteDirectoryName):
+        if not os.path.exists(os.path.dirname(object.key)):
+            os.makedirs(os.path.dirname(object.key))
+        bucket.download_file(object.key,object.key)
+
+# react app 빌드파일 업데이트
+downloadDirectoryFroms3('career-client','build')
+
 migrate = Migrate(app,db)
 manager = Manager(app)
 
